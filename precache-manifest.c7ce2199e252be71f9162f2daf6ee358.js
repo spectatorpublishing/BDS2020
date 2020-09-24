e5257fa85cb827f9b9eed0857163a86a1871da34
HongSenDu <hongsendu@gmail.com>
2020-09-23 21:27:44 -0400

    Updates

---
+++ precache-manifest.c7ce2199e252be71f9162f2daf6ee358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb6c0a9c93e23da3b46bedff4cf83aa",
+    "url": "/BDS2020/index.html"
+  },
+  {
+    "revision": "629e5c1bd9d3768fb470",
+    "url": "/BDS2020/static/css/main.493ff0e7.chunk.css"
+  },
+  {
+    "revision": "329d008791e5f444fee6",
+    "url": "/BDS2020/static/js/2.3d60ebd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BDS2020/static/js/2.3d60ebd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629e5c1bd9d3768fb470",
+    "url": "/BDS2020/static/js/main.a503bfa3.chunk.js"
+  },
+  {
+    "revision": "67d648ece5099e7ef335",
+    "url": "/BDS2020/static/js/runtime-main.8f86af47.js"
+  }
+]);
